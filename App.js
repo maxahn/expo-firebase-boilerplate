65cecaaa9d02332f9a07a731318df6052781f8fa
maxahn <ahnmaximus@gmail.com>
2021-03-19 20:35:55 -0700

    progress on header in home

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
-// import NavigationBar from './src/components/molecules/Navigators/AppNavigator';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
 import BottomNavigationBar from './src/components/molecules/BottomNavigationBar';
 import { FirebaseContext, Firebase } from './src/services/Firebase';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <FirebaseContext.Provider value={new Firebase()}>
-        <BottomNavigationBar />
-      </FirebaseContext.Provider>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <BottomNavigationBar />
+        </FirebaseContext.Provider>
+      </ApplicationProvider>
+    </>
   );
-}
+};
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,79 @@
 import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import {
+  Layout,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+  Icon,
+  MenuItem,
+  OverflowMenu,
+  Divider,
+} from '@ui-kitten/components';
+import { HOME } from '../../constants/routes';
+import capitalize from '../../services/StringUtil';
 
-const Home = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 128,
+  },
+  header: {
+    flexDirection: 'row',
+    backgroundColor: 'blue',
+  },
+  body: {
+    flex: 6,
+    backgroundColor: 'green',
+  },
+});
+
+const BackIcon = (props) => <Icon {...props} name="arrow-back" />;
+const EditIcon = (props) => <Icon {...props} name="edit" />;
+const MenuIcon = (props) => <Icon {...props} name="more-vertical" />;
+
+// const BackAction = () => (<TopNavigationAction icon={BackIcon} />);
+
+const InfoIcon = (props) => <Icon {...props} name="info" />;
+const LogoutIcon = (props) => <Icon {...props} name="log-out" />;
+
+const Home = () => {
+  const [menuVisible, setMenuVisible] = React.useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const renderMenuAction = () => <TopNavigationAction icon={MenuIcon} onPress={toggleMenu} />;
+
+  const renderRightActions = () => (
+    <>
+      <TopNavigationAction icon={EditIcon} />
+      <OverflowMenu anchor={renderMenuAction} visible={menuVisible} onBackdropPress={toggleMenu}>
+        <MenuItem accessoryLeft={InfoIcon} title="About" />
+        <MenuItem accessoryLeft={LogoutIcon} title="Logout" />
+      </OverflowMenu>
+    </>
+  );
+
+  const renderBackAction = () => <TopNavigationAction icon={BackIcon} />;
+
+  return (
+    <SafeAreaView>
+      <TopNavigation
+        alignment="center"
+        title={capitalize(HOME)}
+        accessoryLeft={renderBackAction}
+        accessoryRight={renderRightActions}
+        style={styles.header}
+      />
+      <Divider />
+      <Layout style={styles.container} level="1">
+        {/* <Layout style={styles.body}> */}
+        <Text>Body</Text>
+        {/* </Layout> */}
+      </Layout>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
