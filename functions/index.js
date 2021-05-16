ba60577954bbc8faf1aab37f8077b9dd4360e342
maxahn <ahnmaximus@gmail.com>
2021-05-15 17:14:38 -0700

    create new user signup function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,29 +11,28 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 exports.createNewUserWithUsername = functions.https.onCall((data, context) => {
-  const {username, email, password} = data;
+  const { username, email, password } = data;
   // TODO: check uniqueness of username
-  return admin.auth().createUser({
-    email,
-    emailVerified: false,
-    password,
-    displayName: username,
-  }).catch((err) => {
-    console.log({err});
-    throw new functions.https.HttpsError(
-        "Auth Error:",
-        err
-    );
-  }).then((userRecord) => {
-    return admin.auth().createCustomToken(userRecord.uid);
-  }).then((customToken) => {
-    return customToken;
-  }).catch((err) => {
-    console.log({err});
-    throw new functions.https.HttpsError(
-        "Custom token Error:",
-        err
-    );
-  });
+  return admin
+    .auth()
+    .createUser({
+      email,
+      emailVerified: false,
+      password,
+      displayName: username,
+    })
+    .catch((err) => {
+      console.log({ err });
+      throw new functions.https.HttpsError('Auth Error:', err);
+    })
+    .then((userRecord) => {
+      return admin.auth().createCustomToken(userRecord.uid);
+    })
+    .then((customToken) => {
+      return customToken;
+    })
+    .catch((err) => {
+      console.log({ err });
+      throw new functions.https.HttpsError('Custom token Error:', err);
+    });
 });
-
