7a7904d3d772497ca4b4f1d791b1f31035026ee1
maxahn <ahnmaximus@gmail.com>
2021-05-23 05:08:26 -0700

    changed tasks from array in user doc to a subcollection

--- functions/index.js
+++ functions/index.js
@@ -63,63 +63,88 @@ exports.createFirestoreUser = functions.auth.user().onCreate(async (user) => {
 
 exports.getTasks = functions.https.onCall(async (data, context) => {
   const { uid } = context.auth;
-  if (!uid) throw new functions.https.HttpsError('unauthenticated', 'Request does not have user permissions');
-  const userSnapshot = await admin.firestore().collection('users').where('authUID', '==', uid).get();
+  if (!uid)
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'Request does not have user permissions',
+    );
+  const userSnapshot = await admin
+    .firestore()
+    .collection('users')
+    .where('authUID', '==', uid)
+    .get();
   const userData = userSnapshot.docs[0];
-  if (!userData) throw new functions.https.HttpsError('internal', 'Failed to find authenticated user record.');
-  console.log({tasks: userData.get('tasks')});
-  return userData.get('tasks');
+  if (!userData)
+    throw new functions.https.HttpsError('internal', 'Failed to find authenticated user record.');
+  const tasksQuerySnapshot = await admin
+    .firestore()
+    .collection('users')
+    .doc(userData.id)
+    .collection('tasks').get();
+  const tasks = [];
+  tasksQuerySnapshot.forEach((doc) => {
+    tasks.push(doc.data());
+  });
+  return tasks;
 });
 
 // TODO: move this to another file
 exports.seedUserData = functions.https.onRequest((req, res) => {
-  admin.auth().createUser({
-    email: 'max@max.com',
-    emailVerified: false,
-    password: 'omgwtfbbq',
-    displayName: 'max',
-  }).then(userRecord => {
-    console.log({userRecord});
-  }).then((result) => {
-    res.send(result);
-  }).catch(err => {
-    console.log({err});
-    res.send(err);
-  });
+  admin
+    .auth()
+    .createUser({
+      email: 'max@max.com',
+      emailVerified: false,
+      password: 'omgwtfbbq',
+      displayName: 'max',
+    })
+    .then((userRecord) => {
+      console.log({ userRecord });
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.send(err);
+    });
 });
 
 exports.seedTasks = functions.https.onRequest((req, res) => {
   const usersRef = admin.firestore().collection('users');
-  usersRef.where('displayName', '==', 'max')
+  usersRef
+    .where('displayName', '==', 'max')
     .get()
     .then((querySnapShot) => {
-      const doc = querySnapShot.docs[0]
-      return admin.firestore().collection('users').doc(doc.id).update({
-        tasks: [
-          {
-            title: 'Create day manager app',
-            description: 'Maybe I will finally get stuff done with this',
-            estimatedMinutesToCompleted: 400,
-            isCompleted: false
-          },
-          {
-            title: 'Take out trash',
-            estimatedMinutesToCompleted: 15,
-            isCompleted: true,
-            dateTimeCompleted: new Date().getTime(),
-          },
-          {
-            title: 'Clean room',
-            estimatedMinutesToCompleted: 30,
-            isCompleted: false,
-          },
-        ]
-      });
-    }).then((result) => {
-      console.log('success!!!')
+      const doc = querySnapShot.docs[0];
+      const tasksRef = admin.firestore().collection('users').doc(doc.id).collection('tasks');
+      const promises = [];
+      console.log({uid: doc.id});
+      promises.push(tasksRef.add({
+        title: 'Create day manager app',
+        description: 'Maybe I will finally get stuff done with this',
+        estimatedMinutesToCompleted: 400,
+        isCompleted: false,
+      }));
+      promises.push(tasksRef.add({
+        title: 'Take out trash',
+        estimatedMinutesToCompleted: 15,
+        isCompleted: true,
+        dateTimeCompleted: new Date().getTime(),
+      }));
+      promises.push(tasksRef.add({
+        title: 'Clean room',
+        estimatedMinutesToCompleted: 30,
+        isCompleted: false,
+      }));
+      return Promise.all(promises);
+    })
+    .then((result) => {
+      console.log('success!!!');
       res.send(result);
-    }).catch(err => {
-      console.log({err});
+    })
+    .catch((err) => {
+      console.log({ err });
       res.send(err);
     });
-})
+});
